a0eb1c3cbd5e02da23c7434bf9805dad607eff04
andylithia <thelithcore@gmail.com>
2024-06-23 04:14:47 -0400

    Post Processor Update: RGB565 dithering, gamma correction

--- software/postprocessing/effusus_main.c
+++ software/postprocessing/effusus_main.c
@@ -77,12 +77,6 @@ void write_png(const char* filename, uint16_t* data) {
     png_bytep row = (png_bytep)malloc(3 * PNG_WIDTH * sizeof(png_byte));
     for (int y = 0; y < PNG_HEIGHT; y++) {
         for (int x = 0; x < PNG_WIDTH; x++) {
-            /*
-            uint16_t pixel = data[y * PNG_WIDTH + x];
-            row[3 * x + 0] = (pixel & 0xF800) >> 8;  // Red component
-            row[3 * x + 1] = (pixel & 0x07E0) >> 3;  // Green component
-            row[3 * x + 2] = (pixel & 0x001F) << 3;  // Blue component
-            */
             row [3*x+0] = data[3*(y*PNG_WIDTH+x)+0]>>8;
             row [3*x+1] = data[3*(y*PNG_WIDTH+x)+1]>>8;
             row [3*x+2] = data[3*(y*PNG_WIDTH+x)+2]>>8;
@@ -100,6 +94,7 @@ void write_png(const char* filename, uint16_t* data) {
 }
 
 #include "effusus.h"
+#include "math.h"
 uint8_t fbuf[(CCD_H+1)*CCD_W*2];
 SDL_Surface* swin;
 SDL_Surface* semu;
@@ -118,8 +113,9 @@ SDL_Rect rdst = {
 
 bool savepng = false;
 int main (int argc, char** argv){
+    double gamma = 1.0;
     if(argc<2){
-        printf("Usage: %s filename [--savepng]\n",argv[0]);
+        printf("Usage: %s filename [--savepng] [--gamma=1.0]\n",argv[0]);
         return -1;
     }
     if(argc>2){
@@ -127,6 +123,10 @@ int main (int argc, char** argv){
             if(strcmp(argv[i],"--savepng")==0) {
                 printf("Option: Save PNG Enabled\n");
                 savepng = true;
+            }
+            if(strncmp(argv[i],"--gamma=",8)==0) {
+                gamma = atof(argv[i]+8);
+                printf("Option: Gamma Correction Enabled, gamma=%f\n",gamma);
             }
         }
     }
@@ -199,15 +199,92 @@ int main (int argc, char** argv){
             }
         }
     }
+
     
-    // Copy to Screen
-    // This is lossy, expect to see banding. (Although the image output shouldn't have any banding)
+    // Render to Screen
+    // This is lossy, expect to see banding when dithering is disabled
+    #define DITHER_FLSTEIN
+
+    #if defined(DITHER_FLSTEIN)
+    // Floyd-Steinberg
+    double kernel[] = {
+        0,        0,        0,
+        0,        0,        7.0/16.0,
+        3.0/16.0, 5.0/16.0, 1.0/16.0
+    };
+    printf("Using Floyd-Steinberg Dithering\n");
+    #else
+    double kernel[] = {
+        0,        0,        0,
+        0,        0,        0,
+        0,        0,        0
+    };
+    #endif
+    
+    uint16_t* r_raw;
+    uint16_t* g_raw;
+    uint16_t* b_raw;
+    uint16_t* r_q;
+    uint16_t* g_q;
+    uint16_t* b_q;
+    r_raw = (uint16_t*)malloc(2*SCR_H*SCR_W);
+    g_raw = (uint16_t*)malloc(2*SCR_H*SCR_W);
+    b_raw = (uint16_t*)malloc(2*SCR_H*SCR_W);
+    r_q   = (uint16_t*)malloc(2*SCR_H*SCR_W);
+    g_q   = (uint16_t*)malloc(2*SCR_H*SCR_W);
+    b_q   = (uint16_t*)malloc(2*SCR_H*SCR_W);
+
     for(int y=0;y<SCR_H;y++){
         for(int x=0;x<SCR_W;x++){
-            vram[y*SCR_W+x] = ((raw[4*(y*SCR_W+x)+0]>>(16-5))<<11)&0xF800 | ((raw[4*(y*SCR_W+x)+1]>>(16-5))<<6)&0x07E0 | raw[4*(y*SCR_W+x)+2]>>(16-5)&0x001F;
+            uint16_t r  = raw[4*(y*SCR_W+x)+0];
+            uint16_t g1 = raw[4*(y*SCR_W+x)+1];
+            uint16_t g2 = raw[4*(y*SCR_W+x)+3];
+            uint16_t b  = raw[4*(y*SCR_W+x)+2];
+            uint16_t g  = (g1>>1)+(g2>>1);
+            r_raw[y*SCR_W+x] = r;
+            g_raw[y*SCR_W+x] = g;
+            b_raw[y*SCR_W+x] = b;
+        }
+    }
+    for(int y=0;y<SCR_H;y++){
+        for(int x=0;x<SCR_W;x++){
+            // Quantize
+            r_q[y*SCR_W+x] = r_raw[y*SCR_W+x]&0xF800;
+            g_q[y*SCR_W+x] = g_raw[y*SCR_W+x]&0xF800;
+            b_q[y*SCR_W+x] = b_raw[y*SCR_W+x]&0xF800;
+            // Diffuse Error
+            int r_err = r_raw[y*SCR_W+x] - r_q[y*SCR_W+x];
+            int g_err = g_raw[y*SCR_W+x] - g_q[y*SCR_W+x];
+            int b_err = b_raw[y*SCR_W+x] - b_q[y*SCR_W+x];
+            for(int i=0;i<3;i++){
+                for(int j=0;j<3;j++){
+                    int dx = i-1;
+                    int dy = j-1;
+                    if(x+dx>=0 && x+dx<SCR_W && y+dy>=0 && y+dy<SCR_H){
+                        r_raw[(y+dy)*SCR_W+(x+dx)] += r_err*kernel[j*3+i];
+                        g_raw[(y+dy)*SCR_W+(x+dx)] += g_err*kernel[j*3+i];
+                        b_raw[(y+dy)*SCR_W+(x+dx)] += b_err*kernel[j*3+i];
+                    }
+                }
+            }
+            // gamma correction for preview
+            double delta = 63487.0;
+            uint8_t r_qg  = (uint8_t)(pow((double)r_q[y*SCR_W+x] /delta,1.0/gamma)*31.0);
+            uint8_t g_qg  = (uint8_t)(pow((double)g_q[y*SCR_W+x] /delta,1.0/gamma)*31.0);
+            uint8_t b_qg  = (uint8_t)(pow((double)b_q[y*SCR_W+x] /delta,1.0/gamma)*31.0);
+
+            // Writing to FB
+            vram[y*SCR_W+x] = (r_qg<<11)&0xF800 | (g_qg<<6)&0x07E0 | b_qg&0x001F;
         }
     }
 
+    free(r_q);
+    free(g_q);
+    free(b_q);
+    free(r_raw);
+    free(g_raw);
+    free(b_raw);
+    
 
     SDL_Event event;
 	bool running = true;
@@ -233,45 +310,25 @@ int main (int argc, char** argv){
 
     SDL_SoftStretch(semu,&rsrc,swin,&rdst);
 	SDL_Flip(swin);
-
-    // Save the four channels as separate PNG files.
-    uint16_t* raw_rgb = (uint16_t*)malloc(2*(SCR_W+1)*SCR_H*3);
-    uint16_t* raw_r   = (uint16_t*)malloc(2*(SCR_W+1)*SCR_H*3);
-    uint16_t* raw_g1  = (uint16_t*)malloc(2*(SCR_W+1)*SCR_H*3);
-    uint16_t* raw_b   = (uint16_t*)malloc(2*(SCR_W+1)*SCR_H*3);
-    uint16_t* raw_g2  = (uint16_t*)malloc(2*(SCR_W+1)*SCR_H*3);
-    memset(raw_r,0,sizeof(raw_r));
-    memset(raw_r,0,sizeof(raw_g1));
-    memset(raw_r,0,sizeof(raw_b));
-    memset(raw_r,0,sizeof(raw_g2));
-    for(int y=0;y<SCR_H;y++){
-        for(int x=0;x<SCR_W;x++){
-                int ptr3 = 3*(y*SCR_W+x);
-                int ptr4 = 4*(y*SCR_W+x);
-                raw_rgb[ptr3+0] = raw[ptr4+0];
-                raw_rgb[ptr3+1] = (raw[ptr4+1]>>1)+(raw[ptr4+3]>>1);
-                raw_rgb[ptr3+2] = raw[ptr4+2];
-                raw_r  [ptr3+0] = raw[ptr4+0];
-                raw_g1 [ptr3+1] = raw[ptr4+1];
-                raw_b  [ptr3+2] = raw[ptr4+2];
-                raw_g2 [ptr3+1] = raw[ptr4+3];
-        }
-    }
-
     if(savepng){
+
+        // Save the four channels as separate PNG files.
+        uint16_t* raw_rgb = (uint16_t*)malloc(2*(SCR_W+1)*SCR_H*3);
+        for(int y=0;y<SCR_H;y++){
+            for(int x=0;x<SCR_W;x++){
+                    int ptr3 = 3*(y*SCR_W+x);
+                    int ptr4 = 4*(y*SCR_W+x);
+                    raw_rgb[ptr3+0] = raw[ptr4+0];
+                    raw_rgb[ptr3+1] = (raw[ptr4+1]>>1)+(raw[ptr4+3]>>1);
+                    raw_rgb[ptr3+2] = raw[ptr4+2];
+            }
+        }
         printf("Saving PNG...\n");
         write_png("output.png", raw_rgb);
-        write_png("output_r.png", raw_r);
-        write_png("output_g1.png", raw_g1);
-        write_png("output_b.png", raw_b);
-        write_png("output_g2.png", raw_g2);
+        free(raw_rgb);
     }
+
     free(raw);
-    free(raw_rgb);
-    free(raw_r);
-    free(raw_g1);
-    free(raw_b);
-    free(raw_g2);
 	SDL_Quit();
     printf("Done!\n");
     printf("Gapsize: %ld, Gapshift: %ld\n", gapsize, gapshift);
