1024f5e09bed8f5bc31c6c55e566b60970c82b1d
Wenting Zhang <zephray@outlook.com>
2024-07-25 07:10:01 -0400

    Add standalone FSBL for JTAG flashing purpose

---
+++ firmware/ap/platform/ps7_cortexa9_0/jtag_fsbl_domain/bsp/libsrc/build_configs/gen_bsp/libsrc/coresightps_dcc/src/CMakeFiles/coresightps_dcc.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/wenting/Sitina1/firmware/ap/platform/ps7_cortexa9_0/jtag_fsbl_domain/bsp/libsrc/coresightps_dcc/src/xcoresightpsdcc.c" "libsrc/coresightps_dcc/src/CMakeFiles/coresightps_dcc.dir/xcoresightpsdcc.c.obj" "gcc" "libsrc/coresightps_dcc/src/CMakeFiles/coresightps_dcc.dir/xcoresightpsdcc.c.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ firmware/ap/platform/ps7_cortexa9_0/jtag_fsbl_domain/bsp/libsrc/build_configs/gen_bsp/libsrc/xadcps/src/CMakeFiles/xadcps.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/wenting/Sitina1/firmware/ap/platform/ps7_cortexa9_0/jtag_fsbl_domain/bsp/libsrc/xadcps/src/xadcps.c" "libsrc/xadcps/src/CMakeFiles/xadcps.dir/xadcps.c.obj" "gcc" "libsrc/xadcps/src/CMakeFiles/xadcps.dir/xadcps.c.obj.d"
+  "/home/wenting/Sitina1/firmware/ap/platform/ps7_cortexa9_0/jtag_fsbl_domain/bsp/libsrc/xadcps/src/xadcps_g.c" "libsrc/xadcps/src/CMakeFiles/xadcps.dir/xadcps_g.c.obj" "gcc" "libsrc/xadcps/src/CMakeFiles/xadcps.dir/xadcps_g.c.obj.d"
+  "/home/wenting/Sitina1/firmware/ap/platform/ps7_cortexa9_0/jtag_fsbl_domain/bsp/libsrc/xadcps/src/xadcps_intr.c" "libsrc/xadcps/src/CMakeFiles/xadcps.dir/xadcps_intr.c.obj" "gcc" "libsrc/xadcps/src/CMakeFiles/xadcps.dir/xadcps_intr.c.obj.d"
+  "/home/wenting/Sitina1/firmware/ap/platform/ps7_cortexa9_0/jtag_fsbl_domain/bsp/libsrc/xadcps/src/xadcps_selftest.c" "libsrc/xadcps/src/CMakeFiles/xadcps.dir/xadcps_selftest.c.obj" "gcc" "libsrc/xadcps/src/CMakeFiles/xadcps.dir/xadcps_selftest.c.obj.d"
+  "/home/wenting/Sitina1/firmware/ap/platform/ps7_cortexa9_0/jtag_fsbl_domain/bsp/libsrc/xadcps/src/xadcps_sinit.c" "libsrc/xadcps/src/CMakeFiles/xadcps.dir/xadcps_sinit.c.obj" "gcc" "libsrc/xadcps/src/CMakeFiles/xadcps.dir/xadcps_sinit.c.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
